1056b96fcbec68d021363e0860ac2615a3f80cef
Vinicius Mendes <103964322+ViniciusMendez@users.noreply.github.com>
2022-07-31 14:30:22 -0300

    agora esta trocando o selecionado

---
+++ projeto-pokemon-slider/src/js/index.js
@@ -0,0 +1,28 @@
+
+//OBJETIVO 1 - quando clicarmos na seta avançar temos que moatrar o proximo cartão.
+
+//- passo 1 - pegar o elemento html na seta avançar
+const btAvancar = document.getElementById("btn-avancar");
+const btVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
+//- passo 2 - identificar o click do usuario 
+btAvancar.addEventListener("click",function(){
+    if(cartaoAtual === cartoes.length - 1) return //clausula de guarda.
+
+    cartaoAtual ++ ;
+    cartoes[cartaoAtual].classList.add("selecionado")
+
+    const cartaSelecionado = document.querySelector(".selecionado");
+    cartaSelecionado.classList.remove("selecionado");
+
+});
+
+
+//OBJETIVO 2 - quando clicarmos na seta voltar temos que moatrar o  cartão anterior.
+
+//- passo 1 - pegar o elemento html na seta voltar
+//- passo 2 - identificar o click do usuario 
+//- passo 3 - fazer aparecer o anterior cartão
+//- passo 4 - buscar cartão selecionado e esconder
+
