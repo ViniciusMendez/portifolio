57ee743cc2c3ffa95197405fa55f00b931d7b6f2
Vinicius Mendes <103964322+ViniciusMendez@users.noreply.github.com>
2022-07-31 14:57:21 -0300

    Update index.js

--- projeto-pokemon-slider/src/js/index.js
+++ projeto-pokemon-slider/src/js/index.js
@@ -6,18 +6,25 @@ const btAvancar = document.getElementById("btn-avancar");
 const btVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
+
+function esconderCartaoSelecionado(){
+    const cartaSelecionado = document.querySelector(".selecionado");
+    cartaSelecionado.classList.remove("selecionado");
+}
+
+function mostrarCartao(indiceCartao){
+    cartoes[indiceCartao].classList.add("selecionado")
+}
+
 //- passo 2 - identificar o click do usuario 
 btAvancar.addEventListener("click",function(){
     if(cartaoAtual === cartoes.length - 1) return //clausula de guarda.
 
+    esconderCartaoSelecionado();
     cartaoAtual ++ ;
-    cartoes[cartaoAtual].classList.add("selecionado")
-
-    const cartaSelecionado = document.querySelector(".selecionado");
-    cartaSelecionado.classList.remove("selecionado");
+    mostrarCartao(cartaoAtual);
 
 });
-
 
 //OBJETIVO 2 - quando clicarmos na seta voltar temos que moatrar o  cartão anterior.
 
@@ -25,4 +32,14 @@ btAvancar.addEventListener("click",function(){
 //- passo 2 - identificar o click do usuario 
 //- passo 3 - fazer aparecer o anterior cartão
 //- passo 4 - buscar cartão selecionado e esconder
+
+btVoltar.addEventListener("click",function(){
+
+    if(cartaoAtual === 0) return
+
+    esconderCartaoSelecionado();
+    cartaoAtual -- ;
+    mostrarCartao(cartaoAtual);
+
+});
 
