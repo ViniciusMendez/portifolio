873bda291eac3e5b87d9c927eaaeea99bc7a10ad
Vinicius Mendes <103964322+ViniciusMendez@users.noreply.github.com>
2023-04-10 19:21:02 -0300

    signos e props

---
+++ signos/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ListaDeSignos from './componentes/ListaDeSignos'
+
+function App() {
+  return (
+    <div className="App">
+      <ListaDeSignos/>
+    </div>
+  );
+}
+
+export default App;
